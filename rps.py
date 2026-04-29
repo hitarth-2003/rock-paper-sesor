f07aa18a9dae32a467714df65e0319c55df54b1d
hitarth-2003 <hitarthchoubisa9680@gmail.com>
2026-04-29 15:30:20 +0530

    second commit

--- rps.py
+++ rps.py
@@ -1,2 +1,10 @@
 # Rock, Paper, Scissors — Learn Python with a simple-but-fun game that everybody knows.
+import random
+options=["rock","paper","secisors"]
+person1=random.options
+person2=random.options
 
+if person1(random.options)==person2(random.options):
+    print("stone paper secisor")
+else:
+    print("next chance:")
